b2950f5411aa70cfe508af24f001bd949018c1b2
ghislaineederer <60490177+ghislaineederer@users.noreply.github.com>
2020-03-17 11:26:22 -0700

    Adding more projects from old site

---
+++ Colors/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas(255, 255);
+}
+
+function draw() {
+  background(255, 200, 500);
+}
---
+++ box_with_circle/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(200, 300);
+  background(220);
+
+}
+
+function draw() {
+ background(220);
+  line(0, 0, 200, 300)
+  line(200, 0, 0, 300)
+ fill('rgb(0,255,0)')
+  ellipse(100, 150, 100, 100, 30, 40)
+   stroke('#fae');
+   strokeWeight(20);
+  // noStroke();
+
+}
+
